83a82c126259f3d129028d0ac9e0293ae6cf2c03
Konstantin <59069015+j1yuu@users.noreply.github.com>
2026-05-19 17:44:47 +0300

    [OOP] Module final homework
    * feat: added all the logic for application mvp, hand-tested methods, initial documentation
    
    * fix: changed magazine issueNumber type, overrided toString in child classes, removed publicationsCount decrementation, protected addPublication method from null etc
    
    * refactor: changed output in some places

---
+++ oop/module-hw/src/entities/Magazine.java
@@ -0,0 +1,67 @@
+package entities;
+
+public class Magazine extends Publication {
+  private int issueNumber;
+
+  public Magazine(String title, String author, int year, int issueNumber) {
+    super(title, author, year);
+    this.issueNumber = issueNumber;
+  }
+
+  public int getIssueNumber() {
+    return issueNumber;
+  }
+
+  public void setIssueNumber(int issueNumber) {
+    if (issueNumber < 0) {
+      System.out.println("\nInvalid issue number. Issue number should be greater than 0.");
+      return;
+    }
+
+    this.issueNumber = issueNumber;
+  }
+
+  @Override
+  public String getType() {
+    return PublicationType.MAGAZINE;
+  }
+
+  @Override
+  public void printDetails() {
+    System.out.println();
+    System.out.println("Title: " + getTitle());
+    System.out.println("Author: " + getAuthor());
+    System.out.println("Year: " + getYear());
+    System.out.println("Issue Number: " + getIssueNumber());
+    System.out.println();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    Magazine other = (Magazine) obj;
+    if (issueNumber != other.issueNumber) {
+      return false;
+    }
+    
+    return super.equals(obj);
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + issueNumber;
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "Magazine(title='" + getTitle() + "', author='" + getAuthor() + "', year=" + getYear() + ", issueNumber=" + issueNumber + ")";
+  }
+}
---
+++ oop/module-hw/src/entities/PublicationType.java
@@ -0,0 +1,16 @@
+package entities;
+
+/**
+ * Класс PublicationType.
+ * 
+ * <p>Содержит константы типов публикаций.</p>
+ * 
+ * @param BOOK String BOOK = "Book" - тип книги, {@code public static final}
+ * @param MAGAZINE String MAGAZINE = "Magazine" - тип журнала, {@code public static final}
+ * @param NEWSPAPER String NEWSPAPER = "Newspaper" - тип газеты, {@code public static final}
+ */
+public abstract class PublicationType {
+  public static final String BOOK = "Book";
+  public static final String MAGAZINE = "Magazine";
+  public static final String NEWSPAPER = "Newspaper";
+}
